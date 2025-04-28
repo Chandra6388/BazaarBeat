979f07bc13a195a4a56457b6f715174a661276f2
Chandra6388 <cppatel6388@gmail.com>
2025-04-28 23:41:22 +0530

    Chandra6388

--- frontend/src/components/header/page.js
+++ frontend/src/components/header/page.js
@@ -48,8 +48,7 @@ const Page = () => {
                             </Link>
 
 
-                            
-                        
+
                             <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
                                 <span className="navbar-toggler-icon" />
                             </button>
