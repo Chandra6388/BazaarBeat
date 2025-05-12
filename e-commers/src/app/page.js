1b9647e1f9d7d41379117445518a3a600c982121
Chandra6388 <cppatel6388@gmail.com>
2025-05-12 16:54:26 +0530

    feat: add components to Home page layout

--- e-commers/src/app/page.js
+++ e-commers/src/app/page.js
@@ -1,7 +1,18 @@
 import Navbar from "@/compoents/Navbar";
+import Footer from "@/compoents/Footer";
+import Banner from "@/compoents/Banner";
+import FeaturedProduct from "@/compoents/FeaturedProduct";
+import HomePageProduct from "@/compoents/HomePageProduct";
+import NewsLetter from "@/compoents/NewsLetter";
 export default function Home() {
   return (
      <>
+       <Navbar />
+       <Banner />
+       <FeaturedProduct />
+       <HomePageProduct />
+       <NewsLetter />
+       <Footer />
      </>
   );
 }
