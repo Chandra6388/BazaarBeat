e84178492b2130228c5831ee61a4eb2a545eb0db
Chandra6388 <cppatel6388@gmail.com>
2025-05-12 17:27:44 +0530

    done

--- e-commers/src/app/page.js
+++ e-commers/src/app/page.js
@@ -1,18 +1,24 @@
+'use client'
+
 import Navbar from "@/compoents/Navbar";
 import Footer from "@/compoents/Footer";
 import Banner from "@/compoents/Banner";
 import FeaturedProduct from "@/compoents/FeaturedProduct";
 import HomePageProduct from "@/compoents/HomePageProduct";
 import NewsLetter from "@/compoents/NewsLetter";
+import Header from "@/compoents/Header";
 export default function Home() {
   return (
-     <>
-       <Navbar />
-       <Banner />
-       <FeaturedProduct />
-       <HomePageProduct />
-       <NewsLetter />
-       <Footer />
-     </>
+    <>
+      <Navbar />
+      <div className="px-6 md:px-16 lg:px-32">
+        <Header />
+        <HomePageProduct />
+        <Banner />
+        <FeaturedProduct />
+        <NewsLetter />
+      </div>
+      <Footer />
+    </>
   );
 }
