dfdec992b53b5874e6dc7453107b8fa0c6162595
Chandra6388 <cppatel6388@gmail.com>
2025-05-20 00:44:19 +0530

    done

--- e-commers/src/compoents/ProductCard.jsx
+++ e-commers/src/compoents/ProductCard.jsx
@@ -5,11 +5,9 @@ import { useAppContext } from '@/context/AppContext';
 
 const ProductCard = ({ product }) => {
 
-    console.log("product", product);
     const { currency, router } = useAppContext()
     return (
-        <div
-            onClick={() => { router.push('/product/' + product._id); scrollTo(0, 0) }}
+        <div onClick={() => { router.push('/product/' + product._id); scrollTo(0, 0) }}
             className="flex flex-col items-start gap-0.5 max-w-[200px] w-full cursor-pointer"
         >
             <div className="cursor-pointer group relative bg-gray-500/10 rounded-lg w-full h-52 flex items-center justify-center">
@@ -51,7 +49,7 @@ const ProductCard = ({ product }) => {
 
             <div className="flex items-end justify-between w-full mt-1">
                 <p className="text-base font-medium">{currency}{product.offerPrice}</p>
-                <button className=" max-sm:hidden px-4 py-1.5 text-gray-500 border border-gray-500/20 rounded-full text-xs hover:bg-slate-50 transition">
+                <button className="max-sm:hidden px-4 py-1.5 text-gray-500 border border-gray-500/20 rounded-full text-xs hover:bg-slate-50 transition">
                     Buy now
                 </button>
             </div>
