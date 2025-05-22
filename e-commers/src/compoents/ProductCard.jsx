d0dee412897783e503895fd81a201ff021710aa8
Chandra6388 <cppatel6388@gmail.com>
2025-05-22 22:04:48 +0530

    Enhance ProductCard component: add click functionality for product navigation

--- e-commers/src/compoents/ProductCard.jsx
+++ e-commers/src/compoents/ProductCard.jsx
@@ -7,9 +7,11 @@ const ProductCard = ({ product }) => {
 
     const { currency, router } = useAppContext()
     return (
+
         <div onClick={() => { router.push('/user/product/' + product._id); scrollTo(0, 0) }}
             className="flex flex-col items-start gap-0.5 max-w-[200px] w-full cursor-pointer"
         >
+            
             <div className="cursor-pointer group relative bg-gray-500/10 rounded-lg w-full h-52 flex items-center justify-center">
                 <Image
                     src={product?.image[0]}
